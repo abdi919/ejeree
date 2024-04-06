569b5cc1bdcb32c62424ab8edbc3952c59bc53bf
abdi919 <155192034+abdi919@users.noreply.github.com>
2024-04-06 17:47:28 -0300

    Update one.js

--- one.js
+++ one.js
@@ -7,7 +7,7 @@ toggleBtn.onclick = function(){
     const isOpen = dropDownMenu.classList.contains('open')
      
     toggleBtnIcon.classList = isOpen
-    ? 'fa-solid fa-bars':'fa-solid fa-xmark'
+    ?'fa-solid fa-bars':'fa-solid fa-xmark'
 }
 
 function okk(){
